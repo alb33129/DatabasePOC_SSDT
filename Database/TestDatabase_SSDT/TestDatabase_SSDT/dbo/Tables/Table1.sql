ecfaa74953a639c4add8474e856591c37c03449d
alb33129 <alb33129@gmail.com>
2019-11-13 11:30:03 +0000

    First Branch commit with profile

--- Database/TestDatabase_SSDT/TestDatabase_SSDT/dbo/Tables/Table1.sql
+++ Database/TestDatabase_SSDT/TestDatabase_SSDT/dbo/Tables/Table1.sql
@@ -1,5 +1,8 @@
 ﻿CREATE TABLE [dbo].[Table1] (
-    [Col1]      VARCHAR (100) NULL,
-    [Table1Key] INT           NULL
+    [Col1]      VARCHAR (100)    NULL,
+    [Table1Key] INT              NULL,
+    [col2]      NUMERIC (28, 12) NULL
 );
+
+
 
